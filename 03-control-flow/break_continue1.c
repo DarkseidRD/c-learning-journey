3c88419744bacc0ad5e01f3757e488b8d0b05e79
DarkseidRD <darkseidrd@gmail.com>
2026-03-05 13:45:13 +0000

    Add c lesson

---
+++ 03-control-flow/break_continue1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+//break= Break out of a loop (Stop)
+//continue=Skip current of a loop (Skip)
+for(int i=1;i<=10;i++){
+
+if(i==4){
+	continue; //also you can test break; 
+}
+
+
+	printf("%d\n",i);
+}
+
+	return 0;
+}
